db6492ff45b1dbb58bd57937eed011c0dcf3df8e
lecobf <lecobf@gmail.com>
2026-05-06 14:22:46 -0300

    fix: readiciona useMemo ao import em Pagamentos.jsx

--- nunca-frontend/src/pages/Pagamentos.jsx
+++ nunca-frontend/src/pages/Pagamentos.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { api } from "../api/config";
 import { fmtBRL, fmtDateBR } from "../utils/formatters";
 import Modal from "../components/Modal";
