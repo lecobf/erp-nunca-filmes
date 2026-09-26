5e9823eb320f9ebb10505be492f0b39d8e1ce096
lecobf <lecobf@gmail.com>
2026-09-26 11:06:01 -0300

    fix: remove BOM do ModalServicoCalendario que corrompeu encoding do build

--- nunca-frontend/src/components/calendario/ModalServicoCalendario.jsx
+++ nunca-frontend/src/components/calendario/ModalServicoCalendario.jsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../api/config";
 import Modal from "../Modal";
 import CurrencyInput from "../CurrencyInput";
